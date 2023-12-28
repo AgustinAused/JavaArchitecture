3e2aa141ceacc2e39a03b32d25d160173a89d15c
Agustin Aused <agus.aused@gmail.com>
2023-12-28 19:48:21 -0300

    change name
    Signed-off-by: Agustin Aused <agus.aused@gmail.com>

--- Aplication01/src/main/java/com/arquitecturajava/ControlerBook.java
+++ Aplication01/src/main/java/com/arquitecturajava/ControlerBook.java
@@ -2,9 +2,6 @@ package com.arquitecturajava;
 
 import java.io.IOException;
 import java.util.List;
-
-import com.arquitecturajava.Book;
-
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -13,15 +10,16 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 //omitimos imports
-@WebServlet("/ControladorLibros")
-public class ControladorLibros extends HttpServlet {
+@WebServlet("/ControlerBook")
+public class ControlerBook extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		RequestDispatcher despachador = null;
 		List<Book> listaDeLibros = Book.searchAll();
+		System.out.println(listaDeLibros.isEmpty());
 		List<String> listaDeCategorias = Book.searchAllCategory();
+		System.out.println(listaDeCategorias.isEmpty());
 		request.setAttribute("listaDeLibros", listaDeLibros);
 		request.setAttribute("listaDeCategorias", listaDeCategorias);
 		despachador = request.getRequestDispatcher("ViewBook.jsp");
