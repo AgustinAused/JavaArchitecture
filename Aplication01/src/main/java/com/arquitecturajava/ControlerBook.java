7ac405b5decd1c8f1a3c254c2fe642a4808b2c36
Agustin Aused <agus.aused@gmail.com>
2023-12-30 16:12:04 -0300

    add logic in the book controller
    Signed-off-by: Agustin Aused <agus.aused@gmail.com>

--- Aplication01/src/main/java/com/arquitecturajava/ControlerBook.java
+++ Aplication01/src/main/java/com/arquitecturajava/ControlerBook.java
@@ -1,43 +1,54 @@
 package com.arquitecturajava;
 
-import java.io.IOException;
-import java.util.List;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
 
-//omitimos imports
-@WebServlet("/ControlerBook")
+/**
+ * Servlet implementation class ControlerBook
+ */
 public class ControlerBook extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		RequestDispatcher despachador = null;
-		if(request.getServletPath().equals("/ViewBook.do")) {
+		System.out.println(request.getServletPath());
+		
+		if (request.getServletPath().equals("/ControlerBook/ViewBook")) {
 			List<Book> listaDeLibros = Book.searchAll();
 			System.out.println(listaDeLibros.isEmpty());
 			List<String> listaDeCategorias = Book.searchAllCategory();
 			System.out.println(listaDeCategorias.isEmpty());
 			request.setAttribute("listaDeLibros", listaDeLibros);
 			request.setAttribute("listaDeCategorias", listaDeCategorias);
-			despachador = request.getRequestDispatcher("ViewBook.jsp");	
-		}else if (request.getServletPath().equals("/FormInsertBook.do")) {
-			List<String> listaDeCategorias=null;
+			despachador = request.getRequestDispatcher("/ViewBook.jsp");
+		} else if (request.getServletPath().equals("/ControlerBook/FormInsertBook")) {
+			List<String> listaDeCategorias = null;
 			listaDeCategorias = Book.searchAllCategory();
 			request.setAttribute("listaDeCategorias", listaDeCategorias);
-			despachador = request.getRequestDispatcher("FormInsertBook.jsp");
-		}else{
+			despachador = request.getRequestDispatcher("/FormInsertBook.jsp");
+		} else {
 			String isbn = request.getParameter("isbn");
 			String titulo = request.getParameter("titulo");
 			String categoria = request.getParameter("categoria");
 			var libro = new Book(isbn, titulo, categoria);
 			libro.insert();
-			despachador = request.getRequestDispatcher("ViewBook.do");
-			
+			despachador = request.getRequestDispatcher("/ControlerBook/ViewBook");
+
 		}
 		despachador.forward(request, response);
 	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
 }
