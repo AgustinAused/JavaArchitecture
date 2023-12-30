d3237d5da6d787cfc11eee582d534154d4aef399
Agustin Aused <agus.aused@gmail.com>
2023-12-30 12:04:58 -0300

    Adding functionality to the book controler
    Signed-off-by: Agustin Aused <agus.aused@gmail.com>

--- Aplication01/src/main/java/com/arquitecturajava/ControlerBook.java
+++ Aplication01/src/main/java/com/arquitecturajava/ControlerBook.java
@@ -16,13 +16,28 @@ public class ControlerBook extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		RequestDispatcher despachador = null;
-		List<Book> listaDeLibros = Book.searchAll();
-		System.out.println(listaDeLibros.isEmpty());
-		List<String> listaDeCategorias = Book.searchAllCategory();
-		System.out.println(listaDeCategorias.isEmpty());
-		request.setAttribute("listaDeLibros", listaDeLibros);
-		request.setAttribute("listaDeCategorias", listaDeCategorias);
-		despachador = request.getRequestDispatcher("ViewBook.jsp");
+		if(request.getServletPath().equals("/ViewBook.do")) {
+			List<Book> listaDeLibros = Book.searchAll();
+			System.out.println(listaDeLibros.isEmpty());
+			List<String> listaDeCategorias = Book.searchAllCategory();
+			System.out.println(listaDeCategorias.isEmpty());
+			request.setAttribute("listaDeLibros", listaDeLibros);
+			request.setAttribute("listaDeCategorias", listaDeCategorias);
+			despachador = request.getRequestDispatcher("ViewBook.jsp");	
+		}else if (request.getServletPath().equals("/FormInsertBook.do")) {
+			List<String> listaDeCategorias=null;
+			listaDeCategorias = Book.searchAllCategory();
+			request.setAttribute("listaDeCategorias", listaDeCategorias);
+			despachador = request.getRequestDispatcher("FormInsertBook.jsp");
+		}else{
+			String isbn = request.getParameter("isbn");
+			String titulo = request.getParameter("titulo");
+			String categoria = request.getParameter("categoria");
+			var libro = new Book(isbn, titulo, categoria);
+			libro.insert();
+			despachador = request.getRequestDispatcher("ViewBook.do");
+			
+		}
 		despachador.forward(request, response);
 	}
 }
