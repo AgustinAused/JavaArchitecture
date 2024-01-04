15b1932da526a19b88a9d227a216c2a427b0bed0
Agustin Aused <agus.aused@gmail.com>
2024-01-04 10:34:27 -0300

    refactor code to apply OCP
    Signed-off-by: Agustin Aused <agus.aused@gmail.com>

--- Aplication01/src/main/java/com/javaArchitecture/controller/actions/Action.java
+++ Aplication01/src/main/java/com/javaArchitecture/controller/actions/Action.java
@@ -1,24 +1,30 @@
 package com.javaArchitecture.controller.actions;
 
+import java.lang.reflect.InvocationTargetException;
+
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 public abstract class Action {
 	public abstract String execute(HttpServletRequest req, HttpServletResponse res);
+
 	public static Action getAction(String tipo) {
 		Action action = null;
-		if (tipo.equals("/ControlerBook/ViewBook")) {
-			action = new ViewBookAction();
-		} else if (tipo.equals("/ControlerBook/FormInsertBook")) {
-			action = new FormInsertBookAction();
-		} else if (tipo.equals("/ControlerBook/InsertBook")) {
-			action = new InsertBookAction();
-		} else if (tipo.equals("/ControlerBook/FormEditBook")) {
-			action = new FormEditBookAction();
-		} else if (tipo.equals("/ControlerBook/DeleteBook")) {
-			action = new DeleteBookAction();
-		} else if (tipo.equals("/ControlerBook/SaveBook")) {
-			action = new SaveAction();
+		// Dividir la cadena por "/"
+		String[] partes = tipo.split("/");
+		// Tomar la última parte de la matriz resultante
+		String nombreAccion = partes[partes.length - 1];
+		try {
+			// Obtener el nombre del paquete de la clase Action
+			String packageName = Action.class.getPackageName();
+			// Construir el nombre completo de la clase que se va a instanciar
+			String className = packageName + "." + nombreAccion + "Action";
+			// Utilizar la reflexión para crear una nueva instancia de la clase
+			Class<?> clazz = Class.forName(className);
+			action = (Action) clazz.getDeclaredConstructor().newInstance();
+		} catch (InstantiationException | IllegalAccessException | ClassNotFoundException |
+		         NoSuchMethodException | InvocationTargetException e) {
+		    e.printStackTrace();
 		}
 		return action;
 	}
