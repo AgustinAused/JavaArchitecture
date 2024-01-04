6e4534ecedded545dd65c37595cc50784ed6809a
Agustin Aused <agus.aused@gmail.com>
2024-01-04 10:09:08 -0300

    Refactor code to apply OCP
    Signed-off-by: Agustin Aused <agus.aused@gmail.com>

--- Aplication01/src/main/java/com/javaArchitecture/controller/ControlerBook.java
+++ Aplication01/src/main/java/com/javaArchitecture/controller/ControlerBook.java
@@ -16,21 +16,9 @@ public class ControlerBook extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		RequestDispatcher despachador = null;
-		System.out.println(request.getServletPath());
 		Action action = null;
-		if (request.getServletPath().equals("/ControlerBook/ViewBook")) {
-			action = new ViewBookAction();
-		} else if (request.getServletPath().equals("/ControlerBook/FormInsertBook")) {
-			action = new FormInsertBookAction();
-		} else if (request.getServletPath().equals("/ControlerBook/InsertBook")) {
-			action = new InsertBookAction();
-		} else if (request.getServletPath().equals("/ControlerBook/FormEditBook")) {
-			action = new FormEditBookAction();
-		} else if (request.getServletPath().equals("/ControlerBook/DeleteBook")) {
-			action = new DeleteBookAction();
-		} else if (request.getServletPath().equals("/ControlerBook/SaveBook")) {
-			action = new SaveAction();
-		}
+		String url = request.getServletPath();
+		action = Action.getAction(url);
 		despachador = request.getRequestDispatcher(action.execute(request, response));
 		despachador.forward(request, response);
 	}
--- Aplication01/src/main/java/com/javaArchitecture/controller/actions/Action.java
+++ Aplication01/src/main/java/com/javaArchitecture/controller/actions/Action.java
@@ -5,4 +5,21 @@ import jakarta.servlet.http.HttpServletResponse;
 
 public abstract class Action {
 	public abstract String execute(HttpServletRequest req, HttpServletResponse res);
+	public static Action getAction(String tipo) {
+		Action action = null;
+		if (tipo.equals("/ControlerBook/ViewBook")) {
+			action = new ViewBookAction();
+		} else if (tipo.equals("/ControlerBook/FormInsertBook")) {
+			action = new FormInsertBookAction();
+		} else if (tipo.equals("/ControlerBook/InsertBook")) {
+			action = new InsertBookAction();
+		} else if (tipo.equals("/ControlerBook/FormEditBook")) {
+			action = new FormEditBookAction();
+		} else if (tipo.equals("/ControlerBook/DeleteBook")) {
+			action = new DeleteBookAction();
+		} else if (tipo.equals("/ControlerBook/SaveBook")) {
+			action = new SaveAction();
+		}
+		return action;
+	}
 }
